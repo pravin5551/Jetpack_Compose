14a2000a45b3523d4c5d5a39bede36855dfec94e
MJGSXRX <PawarPravin2@JohnDeere.com>
2024-11-17 17:07:13 +0530

    Practiced on TextField jetpack compose

--- app/src/main/java/com/pravin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pravin/jetpackcompose/MainActivity.kt
@@ -21,7 +21,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,6 +34,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
@@ -40,15 +46,18 @@ import com.pravin.jetpackcompose.ui.theme.JetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
+            var value by remember {
+                mutableStateOf("")
+            }
             JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier.clickable { columnClick() }) {
@@ -58,6 +67,9 @@ class MainActivity : ComponentActivity() {
                         ListOfUsers(listOf("Pravin", "Vilas", "Pawar"))
                         ButtonClick()
                         ComposableImage()
+
+                        TextField(value = value, onValueChange ={newValue -> value = newValue})
+                        Text(text = value)
                     }
 
                 }
