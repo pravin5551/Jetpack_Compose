c4e5afb0e1df4c09ab60ff0ca5884c0c61c58c6b
pravin5551 <pravinvpawar5551@gmail.com>
2025-02-09 18:22:34 +0530

    feat: refactored MainActivity

--- app/src/main/java/com/pravin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pravin/jetpackcompose/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.pravin.jetpackcompose.componants.DisplayScreen
 import com.pravin.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,108 +50,14 @@ class MainActivity : ComponentActivity() {
 
 
         setContent {
-            var value by remember {
-                mutableStateOf("")
-            }
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.inversePrimary
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.clickable { columnClick() }) {
-                        Spacer(Modifier.size(12.dp))
-                        Greeting("Android")
-                        MyComposableFunction("Pravinnn")
-                        ListOfUsers(listOf("Pravin", "Vilas", "Pawar"))
-                        ButtonClick()
-                        ComposableImage()
-
-                        TextField(value = value, onValueChange ={newValue -> value = newValue})
-                        Text(text = value)
-
-                    }
-
+                    DisplayScreen()
                 }
             }
         }
-    }
-
-    @Composable
-    fun ComposableImage() {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Background", modifier = Modifier
-                .size(82.dp)
-                .padding(10.dp)
-                .clip(
-                    CircleShape
-                )
-                .border(width = 12.dp, color = Color.Black, shape = CircleShape),
-            contentScale = ContentScale.Crop
-        )
-    }
-
-    private fun columnClick() {
-        Toast.makeText(applicationContext, "Clicked on column", Toast.LENGTH_LONG).show()
-    }
-
-    @Composable
-    private fun ButtonClick() {
-        Button(
-            onClick = {
-                Toast.makeText(applicationContext, "Clicked on button", Toast.LENGTH_LONG).show()
-            },
-            shape = RoundedCornerShape(size = 20.dp),
-        ) {
-            Text(text = "Composable Button")
-        }
-    }
-
-    @Composable
-    fun ListOfUsers(listOf: List<String>) {
-        for (i in listOf) {
-            Text(text = i)
-        }
-    }
-}
-
-
-var onClick = {}
-
-@Composable
-fun Greeting(
-    name: String, modifier: Modifier = Modifier
-        .clickable { onClick }
-        .background(Color.Blue)
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun MyComposableFunction(name: String) {
-    SelectionContainer {
-        Text(
-            text = name,
-            color = Color.Black,
-            style = TextStyle(textDecoration = TextDecoration.LineThrough),
-            fontSize = 25.sp,
-            textAlign = TextAlign.Center,
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
     }
 }
